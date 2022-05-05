8dd917dbff2feb8689239671f2bd57c83725ccc6
Chen <licnyu@163.com>
2022-05-06 00:27:36 +0200

    1

--- util/pages/umap_page.py
+++ util/pages/umap_page.py
@@ -18,8 +18,7 @@ def umap_page():
 
 
 
-    umap = pd.read_csv(get_file_path("umap.csv",
-                dir_path=f"{get_dir_name(__file__)}/{util_str}/{data_str}",), sep=',')
+    umap = pd.read_csv("/app/tfcs/util/data/umap.csv", sep=',')
 
     umap_usp = pd.read_csv(get_file_path("umap_usp.csv",
                 dir_path=f"{get_dir_name(__file__)}/{util_str}/{data_str}",), sep=',')
