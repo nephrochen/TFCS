6d2aff9684e01735fcf57c8b6acfcd62ea46a172
Chen <licnyu@163.com>
2022-05-15 01:09:35 +0200

    1

--- util/pages/umap_page.py
+++ util/pages/umap_page.py
@@ -97,13 +97,13 @@ def umap_page():
 
     with col1:
         st.write('### Topological Space for indicator')
-        fig = go.FigureWidget(sc_dt_num(umap_org,"surgery"))
+        fig = go.FigureWidget(sc_dt_num(umap,"surgery"))
         fig.update_layout(template='plotly_white',margin=dict(l=0, r=0, b=0, t=0))
         st.plotly_chart(fig, use_container_width=True)
 
     with col2:
         st.write('### Topological Space for Mortality Risk')
-        fig = go.FigureWidget(sc_dt_num(umap_org,"dead"))
+        fig = go.FigureWidget(sc_dt_num(umap,"dead"))
         fig.update_layout(template='plotly_white',margin=dict(l=0, r=0, b=0, t=0))
         st.plotly_chart(fig, use_container_width=True)
 
