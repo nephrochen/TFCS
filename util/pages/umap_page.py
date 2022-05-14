85e3223ee23e9b4b4547c4bbd6080a63c6e636ba
Chen <licnyu@163.com>
2022-05-14 21:55:49 +0200

    1

--- util/pages/umap_page.py
+++ util/pages/umap_page.py
@@ -52,7 +52,8 @@ def umap_page():
         fig = go.FigureWidget(data=[go.Scatter3d(x=u['UMAP1'].values, 
                                                  y=u['UMAP2'].values, 
                                                  z=u['UMAP3'].values, 
-                                                 )])
+                                                mode='markers',
+            marker=dict(size=3,color=u[select_color].values,colorscale='Viridis',  opacity=0.5))])
         fig.update_layout(template='plotly_white',margin=dict(l=0, r=0, b=0, t=0),)
         fig.update_layout(showlegend=True,legend=dict(orientation="h",yanchor="bottom",xanchor="right",) )
         st.plotly_chart(fig, use_container_width=True,template="plotly_dark")
