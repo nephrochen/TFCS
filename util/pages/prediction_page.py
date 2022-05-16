8a2c6bda41e997b8996abdd8430864a47af2af42
Chen <licnyu@163.com>
2022-05-16 23:21:47 +0200

    1

--- util/pages/prediction_page.py
+++ util/pages/prediction_page.py
@@ -16,7 +16,7 @@ import ast
 ##
 def pdt_feature(f_info,f_i,):
     fs = st.selectbox(f_i,ast.literal_eval(f_info.loc[f_i,"value"]), 
-                    #index=ast.literal_eval(f_info.iloc[f_i,"index"])
+                    index=ast.literal_eval(f_info.iloc[f_i,"index"])
                     )
     #f_input.append(fs)
 
