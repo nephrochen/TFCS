904d4eb7d0766eb7b68706e50226475a87a62c34
Chen <licnyu@163.com>
2022-05-20 23:30:24 +0200

    1

--- util/pages/prediction_page.py
+++ util/pages/prediction_page.py
@@ -121,8 +121,9 @@ def prediction_page():
     st.markdown(
         """<style>
         .boxBorder {
-            border: 2px solid #990066;
             padding: 10px;
+            outline: #990066 solid 5px;
+            outline-offset: 5px;
             font-size:25px;
         }</style>
         """, unsafe_allow_html=True) 
