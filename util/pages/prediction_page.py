8677ab2b8d27e673b9c31b2e1315fd954790e96b
Chen <licnyu@163.com>
2022-05-20 23:37:53 +0200

    1

--- util/pages/prediction_page.py
+++ util/pages/prediction_page.py
@@ -119,8 +119,7 @@ def prediction_page():
     st.markdown("---")
    
     st.markdown(
-        """<style>.boxBorder {border: 10px solid #f5e893;font-size:25px;
-        }</style>
+        """<style>.boxBorder {border: 10px solid #f5e893;font-size: 25px;background-color: #ff6600;}</style>
         """, unsafe_allow_html=True) 
     st.markdown('<div class="boxBorder"><font color="RED">Disclaimer: This predictive tool is only for research purposes</font></div>', unsafe_allow_html=True)
     st.write("## Model Perturbation Analysis")
