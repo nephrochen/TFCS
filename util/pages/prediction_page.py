ab295a9f44f022521fbe1ec85ceca3c5e3d13fd3
Chen <licnyu@163.com>
2022-08-06 00:41:17 +0200

    1

--- util/pages/prediction_page.py
+++ util/pages/prediction_page.py
@@ -162,6 +162,10 @@ def prediction_page():
     else:
         explainer = joblib.load('util/models/ts_k_explainer_b.pkl') 
         model = joblib.load('util/models/ts_b.pkl')  
+        print("################################")
+        print(f_input)
+        print("################################")
+        print(f)
         st_shap(shap.plots.waterfall(shap_values_waterfall(explainer,f_input[:-1],f[:-1])), height=500, width=1000)
         with right_p: st.write('## Predict mortality rate: '+str(round( model.predict_proba([f_input])[:, 1][0]*100,2) )+"%")
 
